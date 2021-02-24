cd0b34c046a7a32d6bb3f9f5ce9dadc3d65fd100
Maksim Paderin <maksimpaderin@Maksims-MacBook-Pro.local>
2021-02-25 01:13:06 +0200

    New features

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # python_task
 
-Install BeatifulSoup4 and requests modules
+Prerequisites:
+
+1. To run this app, you need ```python3``` installed. You will also most likely need ```pip``` or ```pip3``` installed.
+2. Install ```BeatifulSoup4``` and ```requests``` modules via pip or pip3. Those are required dependencies.
+3. Run ```python3  script.py``` from the root directory of a project.
--- script.py
+++ script.py
@@ -6,36 +6,39 @@ import json
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-#Configure logger to log timestamp
+#Configure logger to log timestamp in the format of YY-MM-DD h:m:s
 logging.basicConfig(filename='connection_data.log', format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 #Setting up config parser
 with open('config.json') as json_file:
     data = json.load(json_file)
-periodicity = data['variables']['periodicity']
+periodicity = data['app_variables']['periodicity']
+
+def request_data():
+    #Go through all key-value pairs
+    for url, tag in data['websites_and_content_requirements'].items():
+        try:
+            #Send a get request to a server, stop attempt to contact it after 10 seconds
+            response = requests.get(url, timeout = 10)
+            if response.ok:
+                logging.info(f'Web page {url} is accessible')
+                analyze_data(url, tag, response)
+        except requests.exceptions.ConnectionError:
+            logging.info(f'Attempt to connect to {url} produced a connection error. This website is either not accessible or does not exist. No content requirement will be retrieved')
+
+def analyze_data(url, tag, response):
+    #Initialize html parser
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for item in soup.find_all(tag):
+        if tag != 0:
+            logging.info(f'Here is the {tag} tag for {url}: {item.text}')
+        else:
+            logging.info(f'Either {tag} is not specified for {url} or attempt to retrieve {tag} tag has failed')
 
 def time_tracker_wrapper():
     start_time = datetime.now()
-    get_request()
+    request_data()
     logging.info(f'The response was processed in {str((datetime.now() - start_time).microseconds / 1000)} milliseconds')
-
-def get_request():
-
-    for url, tag in data['website_list'].items():
-        #open with GET method
-        response = requests.get(url, timeout = 60)
-        
-        #checker
-        if response.ok:
-            logging.info(f'Web page {url} is accessible')
-            soup = BeautifulSoup(response.text, 'html.parser')
-            for item in soup.find_all(tag):
-                if tag is not None:
-                    logging.info(f'Here is the {tag} tag info: {item.text}')
-                else:
-                    logging.info(f'{tag} is not specified for {url}')
-        else:
-            logging.info(f'The website {url} is not accessible')
     
 
 def main():
